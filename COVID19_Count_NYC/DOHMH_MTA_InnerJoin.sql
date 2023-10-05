78794abbf28683eeb9ebe8184e1bca8d94c3be9e
Ben Salis <134881005+ben-salis@users.noreply.github.com>
2023-10-05 18:05:13 -0400

    Update DOHMH_MTA_InnerJoin.sql

--- COVID19_Count_NYC/DOHMH_MTA_InnerJoin.sql
+++ COVID19_Count_NYC/DOHMH_MTA_InnerJoin.sql
@@ -3,21 +3,21 @@ WITH
 	ridership_reformat AS (
 		SELECT
 			DATE_FORMAT(Month, '%Y-%m') AS mta_date,
-            `Subway Ridership`, `Bus Ridership`
+        		`Subway Ridership`, `Bus Ridership`
 		FROM 
 			MTA_monthly.ridership
 	)
     
 SELECT 
 	`year_month`, monthly_total AS 'Total COVID-19 Cases',
-    `Subway Ridership`, `Bus Ridership`
+	`Subway Ridership`, `Bus Ridership`
 FROM 
 	COVID19_long.nyc_monthly_totals AS cov
     
 INNER JOIN
 	ridership_reformat AS re
 	ON re.mta_date = cov.year_month #shared key. 
-    #Only want pandemic dates from MTA ridership table. 
+	#Only want pandemic dates from MTA ridership table. 
 
 WHERE
 	Classification = "Cases"
