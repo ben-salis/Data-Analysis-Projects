f75cedc0d566b4a5dbfb7bb0375152828d66c6cf
Ben Salis <134881005+ben-salis@users.noreply.github.com>
2023-10-05 18:02:07 -0400

    Add files via upload

---
+++ COVID19_Count_NYC/DOHMH_MTA_InnerJoin.sql
@@ -0,0 +1,23 @@
+#Joining MTA Ridership table to NYC COVID-19 monthly totals table
+WITH 
+	ridership_reformat AS (
+		SELECT
+			DATE_FORMAT(Month, '%Y-%m') AS mta_date,
+            `Subway Ridership`, `Bus Ridership`
+		FROM 
+			MTA_monthly.ridership
+	)
+    
+SELECT 
+	`year_month`, monthly_total AS 'Total COVID-19 Cases',
+    `Subway Ridership`, `Bus Ridership`
+FROM 
+	COVID19_long.nyc_monthly_totals AS cov
+    
+INNER JOIN
+	ridership_reformat AS re
+	ON re.mta_date = cov.year_month #shared key. 
+    #Only want pandemic dates from MTA ridership table. 
+
+WHERE
+	Classification = "Cases"
